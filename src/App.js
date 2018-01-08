6d680aab403a3c2dec2bc6280da89f4383fcc7c4
Brent Guffens <brentguf@users.noreply.github.com>
2018-01-08 21:17:45 +0100

    search bar

--- src/App.js
+++ src/App.js
@@ -2,17 +2,48 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const isSearched = (searchTerm) => {
+  return item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+}
+
 class App extends Component {
+
+  state = {
+    searchTerm: '',
+    list
+  }
+ 
+  onInputChange = (e) => {
+    this.setState({searchTerm: e.target.value});
+  }
+
   render() {
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={this.onInputChange} type="text"/>
+        {
+          list.filter(isSearched(searchTerm)).map(item => <h1 key={item.objectID}>{item.title}</h1>)
+        }
       </div>
     );
   }
